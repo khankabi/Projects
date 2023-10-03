92ca73eeae4b04ff969e543a0fc94f29cb9a465c
Kabir <115586435+khankabi@users.noreply.github.com>
2023-10-03 22:33:27 +0530

    this is login page setup

--- SRMS/index.php
+++ SRMS/index.php
@@ -44,7 +44,65 @@ include('includes/config.php');
     <!-- Header - set the background image for the header in the line below-->
     <header class="py-5 bg-image-full" style="background-image: url('images/landing img.jpg')">
 
-    </header>
+    
+</header>
+<!-- Login panel -->
+<section id="login" class="container-fluid  text-center py-5 pb-5">
+    <div class="container-fluid">
+      <div class="text-center">
+        <h2 class="text-center pr-5 pb-5" style="font-family: 'Ubuntu', sans-serif;">Login</h2>
+      </div>
+      <div class="col-sm-12">
+        <div class="row ">
+          <div class="col-sm-6">
+            <div class=" mr-5">
+
+              <img src="images/background-image.jpg " class="img-fluid" alt=" student or normal use">
+
+              <div class="border border-secondary login-card"
+                style="margin: auto;
+                    margin-top: auto;padding: 20px;z-index: 99;width: 90%;position: relative;margin-top: -46px;background-color: #FFF;">
+                <h5>Student Login</h5>
+                <a href="find-result.php">
+                  <button class="btn btn-info btn-sm">Click Here</button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 ">
+            <div class="mr-5">
+              <img src="images/background-image.jpg" class="img-fluid" alt="doctor login">
+              <div class="border border-secondary login-card"
+                style="margin: auto;
+                    margin-top: auto;padding: 20px;z-index: 99;width: 90%;position: relative;margin-top: -46px;background-color: #FFF;">
+                <h5>Admin login</h5>
+                <a href="admin-login.php" >
+                  <button class="btn btn-info btn-sm">Click Here</button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <!-- <div class="col-sm-4">
+            <div class="mr-5">
+              <img src="images/background-image.jpg" class="img-fluid" alt="admin login">
+
+              <div class="border border-secondary"
+                style="margin: auto;
+                        margin-top: auto;padding: 20px;z-index: 99;width: 90%;position: relative;margin-top: -46px;background-color: #FFF;">
+                <h5>Admin Login</h5>
+                <a href="login/admin.php" target="_blank">
+                  <button class="btn btn-info btn-sm">Click Here</button>
+                </a>
+              </div>
+            </div>
+          </div> -->
+        </div>
+      </div>
+    </div>
+  </section> 
+
+
     <!-- Content section-->
     <section class="py-5 ">
         <div class="container my-5">
